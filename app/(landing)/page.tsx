9c79e2219d9ad5b74d4e31f75dd0b579eccbeec4
LucasJnoub <lucas.jnoub@gmail.com>
2024-04-06 15:45:03 -0300

    adding login and signup button on landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from '@/components/ui/button'
+import { Link } from 'lucide-react'
 import React from 'react'
 
 export default function LandingPage() {
   return (
-    <div>Landing page (Unproctected)</div>
+    <div>
+      <Button>
+        <Link href='/sign-in'>Login</Link>
+      </Button>
+      <Button>
+        <Link href='/sign-up'>Sign-up</Link>
+      </Button>
+    </div>
   )
 }
