bc5d275983f478290c2e95a1e41af920091e5043
LucasJnoub <lucas.jnoub@gmail.com>
2024-04-06 15:47:54 -0300

    fixing button

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,16 +1,16 @@
 import { Button } from '@/components/ui/button'
-import { Link } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 export default function LandingPage() {
   return (
     <div>
-      <Button>
-        <Link href='/sign-in'>Login</Link>
-      </Button>
-      <Button>
-        <Link href='/sign-up'>Sign-up</Link>
-      </Button>
+      <Link href="/sign-in" passHref>
+        <Button>Login</Button>
+      </Link>
+      <Link href="/sign-up" passHref>
+        <Button>Sign-up</Button>
+      </Link>
     </div>
   )
 }
