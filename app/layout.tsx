d9ca75135aad49aa95fc482afb696bbf491200cc
LucasJnoub <lucas.jnoub@gmail.com>
2024-04-19 19:31:26 -0300

    adding original pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Genius",
   description: "AI Plataform  ",
+  manifest:"/manifest.json"
 };
 
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-//teste
+ 
+import withPWA from 'next-pwa';
+const isProduction = process.env.NODE_ENV === 'production';
+ 
+const config = {
+  cacheOnFrontEndNav: true,
+  reloadOnOnline:true,
+  swcMinify: true,
+  disable:false,
+  workboxOptions:{
+    disableDevLogs:true
+  }
+};
+ 
+const nextConfig = withPWA({
+  dest: 'public',
+  disable: !isProduction,
+})(
+  config
+);
+ 
 export default nextConfig;
